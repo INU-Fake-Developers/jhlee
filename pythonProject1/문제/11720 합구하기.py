888beb83299a166ccc5f27383c1a7f119c5c03d7
leejh021023 <80187541+leejh021023@users.noreply.github.com>
2021-03-30 23:24:58 +0900

    Update 11720 합구하기.py

--- pythonProject1/문제/11720 합구하기.py
+++ pythonProject1/문제/11720 합구하기.py
@@ -1,6 +1,6 @@
-a = int(input())
-b = input()
-c = 0
-for i in b:
-    c = c + int(i)
-print(c)
+a = int(input())  # 숫자의 개수
+b = input()   # 숫자의 개수만큼 공백없이 입력
+c = 0  #합출력을 위해 변수 한개를 생성
+for i in  b:
+    c = c + int(i) # 입력한값들을 더함 i부터 b까지
+print(c) 
