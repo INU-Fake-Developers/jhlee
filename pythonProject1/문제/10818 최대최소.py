1f32c21d39c001e3fff20aaa87dd282b7013c62e
leejh021023 <80187541+leejh021023@users.noreply.github.com>
2021-03-30 18:17:44 +0900

    Add files via upload

---
+++ pythonProject1/문제/10818 최대최소.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = list(map(int,input().split()))
+max = -1000000
+min = 1000000
+for i in a:
+  if (i <= min):
+    min = i
+  if (i >= max):
+    maximum = a[i]
+print(min, max, sep=' ')
